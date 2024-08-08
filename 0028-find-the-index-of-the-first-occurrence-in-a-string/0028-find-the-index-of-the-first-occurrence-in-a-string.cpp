e311dece46f357937082156704f99675f4005426
SatyamRaj04 <107751397+SatyamRaj04@users.noreply.github.com>
2024-08-09 04:58:10 +0530

    Time: 3 ms (42.41%), Space: 7.7 MB (59.43%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,9 +5,30 @@ public:
             return -1;
         }
         
-        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+        /*for (int i = 0; i <= haystack.length() - needle.length(); i++) {
             if (haystack.substr(i, needle.length()) == needle) {
                 return i;
+            }
+        }*/
+
+        for(int i = 0; i <= haystack.length() - needle.length(); i++)
+        {
+            if(haystack[i]==needle[0])
+            {
+                int k =0;
+                int flag = i + needle.size() - 1;
+                for(int j=i; j<=flag; j++)
+                {
+                    if(k==needle.size()-1 && haystack[j]==needle[k])
+                    {
+                        return i;
+                    }
+                    else if(haystack[j]!=needle[k])
+                    {
+                        break;
+                    }
+                    k++;
+                }
             }
         }
         
